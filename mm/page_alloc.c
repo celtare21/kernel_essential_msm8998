96626f76d6f8878200b68642182a9709928c8cbb
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2016-03-17 14:17:41 -0700

    /proc/kpageflags: return KPF_BUDDY for "tail" buddy pages
    Currently /proc/kpageflags returns nothing for "tail" buddy pages, which
    is inconvenient when grasping how free pages are distributed.  This
    patch sets KPF_BUDDY for such pages.
    
    With this patch:
    
      $ grep MemFree /proc/meminfo ; tools/vm/page-types -b buddy
      MemFree:         3134992 kB
                   flags      page-count       MB  symbolic-flags                     long-symbolic-flags
      0x0000000000000400          779272     3044  __________B_______________________________ buddy
      0x0000000000000c00            4385       17  __________BM______________________________ buddy,mmap
                   total          783657     3061
    
    783657 pages is 3134628 kB (roughly consistent with the global counter,)
    so it's OK.
    
    [akpm@linux-foundation.org: update comment, per Naoya]
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Reviewed-by: Vladimir Davydov <vdavydov@virtuozzo.com>>
    Cc: Konstantin Khlebnikov <koct9i@gmail.com>
    Cc: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -7127,7 +7127,6 @@ __offline_isolated_pages(unsigned long start_pfn, unsigned long end_pfn)
 }
 #endif
 
-#ifdef CONFIG_MEMORY_FAILURE
 bool is_free_buddy_page(struct page *page)
 {
 	struct zone *zone = page_zone(page);
@@ -7146,4 +7145,3 @@ bool is_free_buddy_page(struct page *page)
 
 	return order < MAX_ORDER;
 }
-#endif
