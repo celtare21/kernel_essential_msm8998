c1d658fbd5b680f53bb5405eebfc525d7238f4d3
Vlastimil Babka <vbabka@suse.cz>
2016-10-07 16:57:57 -0700

    mm, compaction: use proper alloc_flags in __compaction_suitable()
    The __compaction_suitable() function checks the low watermark plus a
    compact_gap() gap to decide if there's enough free memory to perform
    compaction.  This check uses direct compactor's alloc_flags, but that's
    wrong, since these flags are not applicable for freepage isolation.
    
    For example, alloc_flags may indicate access to memory reserves, making
    compaction proceed, and then fail watermark check during the isolation.
    
    A similar problem exists for ALLOC_CMA, which may be part of
    alloc_flags, but not during freepage isolation.  In this case however it
    makes sense to use ALLOC_CMA both in __compaction_suitable() and
    __isolate_free_page(), since there's actually nothing preventing the
    freepage scanner to isolate from CMA pageblocks, with the assumption
    that a page that could be migrated once by compaction can be migrated
    also later by CMA allocation.  Thus we should count pages in CMA
    pageblocks when considering compaction suitability and when isolating
    freepages.
    
    To sum up, this patch should remove some false positives from
    __compaction_suitable(), and allow compaction to proceed when free pages
    required for compaction reside in the CMA pageblocks.
    
    Link: http://lkml.kernel.org/r/20160810091226.6709-10-vbabka@suse.cz
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Tested-by: Lorenzo Stoakes <lstoakes@gmail.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Rik van Riel <riel@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2506,7 +2506,7 @@ int __isolate_free_page(struct page *page, unsigned int order)
 		/* Obey watermarks as if the page was being allocated */
 		watermark = low_wmark_pages(zone) + (1 << order);
 		if (!is_migrate_cma(mt) &&
-		    !zone_watermark_ok(zone, 0, watermark, 0, 0))
+		    !zone_watermark_ok(zone, 0, watermark, 0, ALLOC_CMA))
 			return 0;
 
 		__mod_zone_freepage_state(zone, -(1UL << order), mt);
